122c0019c32b7fa4e59755ed0af93a51a5248de8
Micaela Nilsson <micaela.nilsson85@gmail.com>
2023-12-31 13:19:17 +0100

    Test contactservice

--- AddressBookNo2/Services/ContactService.cs
+++ AddressBookNo2/Services/ContactService.cs
@@ -1,4 +1,5 @@
 ﻿
+using AddressBookNo2.Interfaces;
 using AddressBookNo2.Models;
 
 
@@ -10,6 +11,8 @@ public class ContactService
     private List<Contact> _contacts = [];
 
     public Contact CurrentContact { get; set; } = null!;
+
+    
     public void Add(Contact contact)
     {
         _contacts.Add(contact);
@@ -43,65 +46,3 @@ public class ContactService
    
 }
 
-//private readonly string filePath;
-
-//public ContactService(string filePath)
-//{
-//    this.filePath = filePath;
-//    LoadContacts(); // Ladda kontakter när tjänsten skapas
-//}
-
-//// Lägg till en kontakt i listan
-//public void AddContact(Contact contact)
-//{
-//    if (contacts == null)
-//    {
-//        contacts = new List<Contact>();
-//    }
-//    contacts.Add(contact); // Spara kontakter när en ny kontakt läggs till
-//}
-
-//public void RemoveContactByEmail(string email)
-//{
-//    if (contacts != null)
-//    {
-//        contacts.RemoveAll(c => c.Email == email);
-//        SaveContacts();
-//    }
-//}
-
-//// Ladda kontakter från JSON-filen
-//private void LoadContacts()
-//{
-//    if (File.Exists(filePath))
-//    {
-//        string json = File.ReadAllText(filePath);
-//        contacts = JsonSerializer.Deserialize<List<Contact>>(json);
-//    }
-//    else
-//    {
-//        contacts = new List<Contact>();
-//    }
-//}
-
-//// Spara kontakter till JSON-filen
-//private void SaveContacts()
-//{
-//    string json = JsonSerializer.Serialize(contacts);
-//    File.WriteAllText(filePath, json);
-//}
-
-//public void UpdateContact(Contact contact)
-//{
-//    throw new NotImplementedException();
-//}
-
-//public List<Contact> GetAllContacts()
-//{
-//    throw new NotImplementedException();
-//}
-
-//public Contact GetContactByEmail(string email)
-//{
-//    throw new NotImplementedException();
-//}
---
+++ ClassLibrary.Tests/ContactService_Tests.cs
@@ -0,0 +1,91 @@
+﻿
+using AddressBookNo2.Models;
+using AddressBookNo2.Services;
+
+
+namespace ClassLibrary.Tests;
+
+public class ContactService_Tests
+{
+    [Fact]
+    public void Add_ShouldAddContactToList()
+    {
+        // Arrange
+        var contactService = new ContactService();
+        var contact = new Contact();
+
+        // Act
+        contactService.Add(contact);
+
+        // Assert
+        var contacts = contactService.GetAll();
+        Assert.Contains(contact, contacts);
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnAllContacts()
+    {
+        // Arrange
+        var contactService = new ContactService();
+        var contact1 = new Contact();
+        var contact2 = new Contact();
+        contactService.Add(contact1);
+        contactService.Add(contact2);
+
+        // Act
+        var contacts = contactService.GetAll();
+
+        // Assert
+        Assert.Contains(contact1, contacts);
+        Assert.Contains(contact2, contacts);
+    }
+
+    [Fact]
+    public void UpdateContact_ShouldUpdateContact_WhenContactExists()
+    {
+        // Arrange
+        var contactService = new ContactService();
+        var originalContact = new Contact();
+        var updatedContact = new Contact
+        {
+            Id = originalContact.Id,
+            FirstName = "Updated",
+            LastName = "Doe",
+            Email = "updated@example.com",
+            Phone = "555-555-5555",
+            Address = "456 Updated St"
+        };
+        contactService.Add(originalContact);
+
+        // Act
+        contactService.UpdateContact(updatedContact);
+
+        // Assert
+        var contacts = contactService.GetAll();
+        var updatedContactInList = contacts.FirstOrDefault(c => c.Id == updatedContact.Id);
+        Assert.NotNull(updatedContactInList);
+        Assert.Equal(updatedContact.FirstName, updatedContactInList.FirstName);
+        Assert.Equal(updatedContact.LastName, updatedContactInList.LastName);
+        Assert.Equal(updatedContact.Email, updatedContactInList.Email);
+        Assert.Equal(updatedContact.Phone, updatedContactInList.Phone);
+        Assert.Equal(updatedContact.Address, updatedContactInList.Address);
+    }
+
+    [Fact]
+    public void RemoveContact_ShouldRemoveContact_WhenContactExists()
+    {
+        // Arrange
+        var contactService = new ContactService();
+        var contact = new Contact();
+        contactService.Add(contact);
+
+        // Act
+        contactService.Remove(contact);
+
+        // Assert
+        var contacts = contactService.GetAll();
+        Assert.DoesNotContain(contact, contacts);
+    }
+}
+
+
